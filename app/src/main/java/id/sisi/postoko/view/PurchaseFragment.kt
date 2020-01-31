9b98f085056936ddfc05d132c69f8b807b5e0df6
Abdurrouf <roufroufrouf@gmail.com>
2020-01-31 08:56:40 +0700

    POS-121 #time 90m tampilan search & list product

--- app/src/main/java/id/sisi/postoko/view/PurchaseFragment.kt
+++ app/src/main/java/id/sisi/postoko/view/PurchaseFragment.kt
@@ -5,8 +5,11 @@ import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import androidx.recyclerview.widget.GridLayoutManager
 
 import id.sisi.postoko.R
+import id.sisi.postoko.adapter.ListProductAdapter
+import kotlinx.android.synthetic.main.fragment_purchase.*
 
 class PurchaseFragment : Fragment() {
     override fun onCreateView(
@@ -17,6 +20,14 @@ class PurchaseFragment : Fragment() {
         activity?.title = getString(R.string.txt_purchase)
         val view = inflater.inflate(R.layout.fragment_purchase, container, false)
         return view
+    }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+
+        rv_list_product?.layoutManager = GridLayoutManager(this.context, 2)
+        rv_list_product?.setHasFixedSize(false)
+        rv_list_product?.adapter = ListProductAdapter()
     }
 
     companion object {
