0abf1818173301f8ba88991f177c68e9ca70f2e9
Abdurrouf <roufroufrouf@gmail.com>
2020-02-19 19:14:11 +0700

    POSMA-35 #time 2h
    +15 Penambahan teks status
    +60 Layouting item sesuai mockup
    +5 Pembuatan model sales sesuai return json, menggunakan generator
    +20 Pembuatan extension, konversi dari number ke rupiah
    +20 Pembuatan extension, konversi dari string ke tanggal
    - kategori sesuai tab & filter
    - cara konversi dari status menggunakan enum

--- app/src/main/java/id/sisi/postoko/adapter/ListSalesAdapter.kt
+++ app/src/main/java/id/sisi/postoko/adapter/ListSalesAdapter.kt
@@ -6,13 +6,17 @@ import android.view.ViewGroup
 import androidx.recyclerview.widget.RecyclerView
 import id.sisi.postoko.R
 import id.sisi.postoko.model.Sales
-import kotlin.random.Random
+import kotlinx.android.synthetic.main.list_item_sales_booking.view.*
+import java.text.NumberFormat
+import java.text.SimpleDateFormat
+import java.util.*
+
 
 class ListSalesAdapter(private var sales: List<Sales>? = arrayListOf()) : RecyclerView.Adapter<ListSalesAdapter.SalesViewHolder>() {
 
     override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): SalesViewHolder {
         val view =
-            LayoutInflater.from(parent.context).inflate(R.layout.list_item_order, parent, false)
+            LayoutInflater.from(parent.context).inflate(R.layout.list_item_sales_booking, parent, false)
 
         return SalesViewHolder(view)
     }
@@ -22,12 +26,20 @@ class ListSalesAdapter(private var sales: List<Sales>? = arrayListOf()) : Recycl
     }
 
     override fun onBindViewHolder(holder: SalesViewHolder, position: Int) {
-        holder.bind("")
+        holder.bind(sales?.get(position))
     }
 
     class SalesViewHolder(itemView: View) : RecyclerView.ViewHolder(itemView) {
 
-        fun bind(value: String) {
+        fun bind(sale: Sales?) {
+            sale?.let {
+                itemView.tv_sales_reference_no?.text = it.reference_no
+                itemView.tv_sales_date?.text = " ${it.date.toDisplayDate()}"
+                itemView.tv_sales_delevery_status?.text = itemView.context.getText(it.delivery_status.toDisplayStatus())
+                itemView.tv_sales_payment_status?.text = itemView.context.getText(it.payment_status.toDisplayStatus())
+                itemView.tv_sales_total_price?.text = it.grand_total.toCurrencyID()
+                itemView.tv_sales_detail?.text = "Lihat ${it.total_items} Rincian Item"
+            }
         }
     }
 
@@ -35,4 +47,64 @@ class ListSalesAdapter(private var sales: List<Sales>? = arrayListOf()) : Recycl
         sales = newMasterData
         notifyDataSetChanged()
     }
+}
+
+fun String.toDisplayStatus(): Int {
+    var idString = 0
+    if (this == "pending") {
+        idString = R.string.txt_status_pending
+    } else if (this == "confirmed") {
+        idString = R.string.txt_status_confirmed
+    } else if (this == "reserved") {
+        idString = R.string.txt_status_reserved
+    } else if (this == "closed") {
+        idString = R.string.txt_status_closed
+    } else if (this == "canceled") {
+        idString = R.string.txt_status_canceled
+    } else if (this == "partial") {
+        idString = R.string.txt_status_partial
+    } else if (this == "due") {
+        idString = R.string.txt_status_due
+    } else if (this == "waiting") {
+        idString = R.string.txt_status_waiting
+    } else if (this == "paid") {
+        idString = R.string.txt_status_paid
+    } else if (this == "done") {
+        idString = R.string.txt_status_done
+    } else if (this == "packing") {
+        idString = R.string.txt_status_packing
+    } else if (this == "delivering") {
+        idString = R.string.txt_status_delivering
+    } else if (this == "delivered") {
+        idString = R.string.txt_status_delivered
+    } else if (this == "returned") {
+        idString = R.string.txt_status_returned
+    } else if (this == "sent") {
+        idString = R.string.txt_status_sent
+    } else if (this == "completed") {
+        idString = R.string.txt_status_completed
+    } else if (this == "received") {
+        idString = R.string.txt_status_received
+    }
+    return idString
+}
+
+fun String.toDisplayDate(): String {
+    try {
+        val dateInFormat = SimpleDateFormat("yyyy-MM-dd HH:mm:ss", Locale.US)
+        val dateOutFormat = SimpleDateFormat("dd MMM yy", Locale("id", "ID"))
+        dateInFormat.parse(this)?.let {
+            return dateOutFormat.format(it)
+        }
+    } catch (e: Exception) { }
+
+    return this
+}
+
+fun Number.toCurrency(): String {
+    return NumberFormat.getCurrencyInstance(Locale.getDefault()).format(this)
+}
+
+fun Number.toCurrencyID(): String {
+    return NumberFormat.getCurrencyInstance(Locale("id", "ID")).format(this)
 }
