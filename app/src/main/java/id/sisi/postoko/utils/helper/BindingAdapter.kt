ae2d2dc5aeef51681d0ce2d8bad00dc289ec7b82
Abdurrouf <roufroufrouf@gmail.com>
2020-03-06 17:03:44 +0700

    POSMA-42 #time 20m melanjutkan proses data binding

--- app/src/main/java/id/sisi/postoko/utils/helper/BindingAdapter.kt
+++ app/src/main/java/id/sisi/postoko/utils/helper/BindingAdapter.kt
@@ -4,8 +4,20 @@ import android.view.View
 import android.widget.TextView
 import androidx.databinding.BindingAdapter
 import id.sisi.postoko.utils.extensions.toCurrencyID
+import id.sisi.postoko.utils.extensions.toDisplayDate
+import id.sisi.postoko.utils.extensions.toDisplayDateFromDO
 
 @BindingAdapter("currencyID")
 fun TextView.bindCurrencyID(amount: String?) {
     text = amount?.toCurrencyID()
+}
+
+@BindingAdapter("displayDateOnly")
+fun TextView.bindDisplayDateOnly(date: String?) {
+    text = date?.toDisplayDateFromDO()
+}
+
+@BindingAdapter("displayDateTime")
+fun TextView.bindDisplayDateTime(date: String?) {
+    text = date?.toDisplayDate()
 }
