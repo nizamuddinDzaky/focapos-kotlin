09efb0723644de17dca7924a991497998dac097b
Abdurrouf <roufroufrouf@gmail.com>
2020-03-10 17:05:21 +0700

    POSMA-83 #time 4h riset deteksi scroll untuk pagination

--- app/src/main/java/id/sisi/postoko/adapter/ListGoodReceivedAdapter.kt
+++ app/src/main/java/id/sisi/postoko/adapter/ListGoodReceivedAdapter.kt
@@ -20,8 +20,26 @@ class ListGoodReceivedAdapter(
     private var goodsReceived: List<GoodReceived>? = arrayListOf(),
     private var status: GoodReceiveStatus = DELIVERING,
     private var listener: (GoodReceived?) -> Unit = {}
-) : RecyclerView.Adapter<ListGoodReceivedAdapter.DetailGoodReceivedViewHolder>() {
+) : RecyclerView.Adapter<ListGoodReceivedAdapter.ViewHolder>() {
 
+    companion object {
+        val ITEM_VIEW_TYPE_CONTENT = 1
+        val ITEM_VIEW_TYPE_LOADING = 2
+    }
+
+    private var isLoading = false
+
+//    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): ViewHolder {
+//        val layoutInflater = LayoutInflater.from(parent.context)
+//        return when (viewType) {
+//            ITEM_VIEW_TYPE_CONTENT -> DetailGoodReceivedViewHolder(
+//                layoutInflater.inflate(R.layout.list_item_gr, null)
+//            )
+//            else -> LoadingViewHolder(
+//                layoutInflater.inflate(R.layout.list_item_data_loading, null)
+//            )
+//        }
+//    }
     override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): DetailGoodReceivedViewHolder {
         val view =
             LayoutInflater.from(parent.context).inflate(R.layout.list_item_gr, parent, false)
@@ -33,13 +51,38 @@ class ListGoodReceivedAdapter(
         return goodsReceived?.size ?: 0
     }
 
-    override fun onBindViewHolder(holder: DetailGoodReceivedViewHolder, position: Int) {
-        holder.bind(goodsReceived?.get(position), status, listener)
+    override fun onBindViewHolder(holder: ViewHolder, position: Int) {
+        if (!isLastItem(position)) {
+            (holder as? DetailGoodReceivedViewHolder)?.bind(
+                goodsReceived?.get(position),
+                status,
+                listener
+            )
+        }
     }
 
-    class DetailGoodReceivedViewHolder(itemView: View) : RecyclerView.ViewHolder(itemView) {
+    override fun getItemViewType(position: Int): Int {
+        if (goodsReceived?.size ?: 0 < 10) {
+            return ITEM_VIEW_TYPE_CONTENT
+        }
+        return if (isLastItem(position)) ITEM_VIEW_TYPE_LOADING else ITEM_VIEW_TYPE_CONTENT
+    }
 
-        fun bind(goodReceived: GoodReceived?, status: GoodReceiveStatus, listener: (GoodReceived?) -> Unit) {
+    private fun isLastItem(position: Int): Boolean {
+        return goodsReceived?.size?.minus(1) == position
+    }
+
+    open class ViewHolder(itemView: View) : RecyclerView.ViewHolder(itemView)
+
+    class LoadingViewHolder(itemView: View) : ViewHolder(itemView)
+
+    class DetailGoodReceivedViewHolder(itemView: View) : ViewHolder(itemView) {
+
+        fun bind(
+            goodReceived: GoodReceived?,
+            status: GoodReceiveStatus,
+            listener: (GoodReceived?) -> Unit
+        ) {
             goodReceived?.let {
                 itemView.tv_good_received_do_number?.text = it.no_do
                 itemView.tv_good_received_so_number?.text = it.no_so
@@ -60,7 +103,11 @@ class ListGoodReceivedAdapter(
     }
 
     fun updateGoodsReceivedData(newTransactionsData: List<GoodReceived>?) {
-        goodsReceived = newTransactionsData
+        val tempList = newTransactionsData?.toMutableList()
+        logE("isi sebelum ${tempList?.size}")
+        tempList?.add(GoodReceived())
+        goodsReceived = tempList
+        logE("isi sesudah ${goodsReceived?.size}")
         notifyDataSetChanged()
     }
 }
