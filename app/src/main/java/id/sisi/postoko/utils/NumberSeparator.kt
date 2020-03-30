ceeee44ab8e4c84a9fc39033fcad87117d9322ca
nizam31 <nizamuddin.dzaky@gmail.com>
2020-03-30 13:05:02 +0700

    POSMA-42 #time 2h reserch number format dan implementasi di android POS untuk input number

---
+++ app/src/main/java/id/sisi/postoko/utils/NumberSeparator.kt
@@ -0,0 +1,52 @@
+package id.sisi.postoko.utils
+
+import android.text.Editable
+import android.text.TextWatcher
+import android.widget.EditText
+import java.text.DecimalFormat
+import java.text.NumberFormat
+import java.util.*
+
+class NumberSeparator {
+    fun onTextChangedListener(editText : EditText): TextWatcher? {
+        return object : TextWatcher {
+            override fun beforeTextChanged(
+                s: CharSequence,
+                start: Int,
+                count: Int,
+                after: Int
+            ) {
+            }
+
+            override fun onTextChanged(
+                s: CharSequence,
+                start: Int,
+                before: Int,
+                count: Int
+            ) {
+            }
+
+            override fun afterTextChanged(s: Editable) {
+                editText.removeTextChangedListener(this)
+                try {
+                    var originalString = s.toString()
+                    val longval: Long
+                    if (originalString.contains(",")) {
+                        originalString = originalString.replace(",".toRegex(), "")
+                    }
+                    longval = originalString.toLong()
+                    val formatter: DecimalFormat =
+                        NumberFormat.getInstance(Locale.US) as DecimalFormat
+                    formatter.applyPattern("#,###,###,###")
+                    val formattedString: String = formatter.format(longval)
+                    //setting text after format to EditText
+                    editText.setText(formattedString)
+                    editText.setSelection(editText.text.length)
+                } catch (nfe: NumberFormatException) {
+                    nfe.printStackTrace()
+                }
+                editText.addTextChangedListener(this)
+            }
+        }
+    }
+}
