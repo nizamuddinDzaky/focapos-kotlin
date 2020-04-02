248f329595da2173dda0f7ef11020f79167be87a
Abdurrouf <roufroufrouf@gmail.com>
2020-04-02 13:53:19 +0700

    POSMA-127 #time 60m handle halaman profile + convert status male female ke teks + handle refresh ketika ada data di update

--- app/src/main/java/id/sisi/postoko/view/ui/profile/ProfileViewModel.kt
+++ app/src/main/java/id/sisi/postoko/view/ui/profile/ProfileViewModel.kt
@@ -7,6 +7,7 @@ import id.sisi.postoko.MyApp
 import id.sisi.postoko.model.User
 import id.sisi.postoko.network.ApiServices
 import id.sisi.postoko.utils.KEY_FORCA_TOKEN
+import id.sisi.postoko.utils.KEY_ID_USER
 import id.sisi.postoko.utils.extensions.exe
 import id.sisi.postoko.utils.extensions.logE
 import id.sisi.postoko.utils.extensions.tryMe
@@ -14,12 +15,13 @@ import id.sisi.postoko.utils.extensions.tryMe
 class ProfileViewModel : ViewModel() {
     private val user = MutableLiveData<User?>()
     private var isExecute = MutableLiveData<Boolean>()
+    private var isSuccessUpdate = MutableLiveData<Boolean>()
 
     init {
         getUserProfile()
     }
 
-    private fun getUserProfile() {
+    fun getUserProfile() {
         isExecute.postValue(true)
         val headers = mutableMapOf(KEY_FORCA_TOKEN to (MyApp.prefs.posToken ?: ""))
         ApiServices.getInstance()?.getProfile(headers)?.exe(
@@ -31,8 +33,38 @@ class ProfileViewModel : ViewModel() {
                 isExecute.postValue(false)
                 if (response.isSuccessful) {
                     tryMe {
-                        logE("tes ${response.body()?.data?.user?.username}")
-                        user.postValue(response.body()?.data?.user)
+                        val newUser = response.body()?.data?.user
+                        newUser?.companyData = response.body()?.data?.company
+                        user.postValue(newUser)
+                    }
+                } else {
+                    isExecute.postValue(true)
+                }
+            }
+        )
+    }
+
+    fun putUserProfile(body: MutableMap<String, String>, idUser: String) {
+        if (body.isEmpty()) {
+            logE("body is empty, force cancel call api.")
+            return
+        }
+        isExecute.postValue(true)
+        val headers = mutableMapOf(KEY_FORCA_TOKEN to (MyApp.prefs.posToken ?: ""))
+        val params = mutableMapOf(KEY_ID_USER to idUser)
+        ApiServices.getInstance()?.putProfile(headers, params, body)?.exe(
+            onFailure = { _, _ ->
+                isExecute.postValue(true)
+                user.postValue(null)
+            },
+            onResponse = { _, response ->
+                isExecute.postValue(false)
+                if (response.isSuccessful) {
+                    tryMe {
+                        val newUser = response.body()?.data?.user
+                        newUser?.companyData = response.body()?.data?.company
+                        user.postValue(newUser)
+                        isSuccessUpdate.postValue(true)
                     }
                 } else {
                     isExecute.postValue(true)
@@ -45,6 +77,8 @@ class ProfileViewModel : ViewModel() {
         isExecute.postValue(true)
         return isExecute
     }
+
+    internal fun getIsSuccessUpdatee() = isSuccessUpdate
 
     internal fun getUser(): LiveData<User?> {
         return user
