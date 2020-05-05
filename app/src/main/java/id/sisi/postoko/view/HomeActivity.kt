9cb939db3624670358ed519ee845209a935dc1c5
Asthok Aripasola <arpasola22@gmail.com>
2020-05-05 10:21:32 +0700

    get token firebase FCM

--- app/src/main/java/id/sisi/postoko/view/HomeActivity.kt
+++ app/src/main/java/id/sisi/postoko/view/HomeActivity.kt
@@ -13,6 +13,7 @@ import androidx.lifecycle.Observer
 import androidx.lifecycle.ViewModelProvider
 import com.github.javiersantos.appupdater.AppUpdater
 import com.github.javiersantos.appupdater.enums.UpdateFrom
+import com.google.firebase.iid.FirebaseInstanceId
 import id.sisi.postoko.MyApp
 import id.sisi.postoko.R
 import id.sisi.postoko.model.Customer
@@ -79,6 +80,9 @@ class HomeActivity : BaseActivity() {
         appUpdater.setDisplay(com.github.javiersantos.appupdater.enums.Display.DIALOG)
         appUpdater.setUpdateFrom(UpdateFrom.GOOGLE_PLAY)
         appUpdater.start()
+
+        val firebaseToken = FirebaseInstanceId.getInstance().getToken()
+        Toast.makeText(this, "FCM Registration Token: $firebaseToken", Toast.LENGTH_LONG).show()
     }
 
     override fun onResume() {
