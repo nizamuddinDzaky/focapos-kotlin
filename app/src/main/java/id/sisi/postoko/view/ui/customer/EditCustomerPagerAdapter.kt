6d4eb9870b73f7d9a534bf928484def393bba795
asthok_aripasola <arpasola22@gmail.com>
2020-05-28 19:23:36 +0700

    POSMA-230 #time 10m Memperbaiki beberapa typo

--- app/src/main/java/id/sisi/postoko/view/ui/customer/EditCustomerPagerAdapter.kt
+++ app/src/main/java/id/sisi/postoko/view/ui/customer/EditCustomerPagerAdapter.kt
@@ -1,4 +1,28 @@
 package id.sisi.postoko.view.ui.customer
 
-class EditCustomerPagerAdapter {
+import androidx.fragment.app.Fragment
+import androidx.fragment.app.FragmentManager
+import androidx.fragment.app.FragmentPagerAdapter
+
+class EditCustomerPagerAdapter(fm: FragmentManager) : FragmentPagerAdapter(fm, BEHAVIOR_RESUME_ONLY_CURRENT_FRAGMENT) {
+    private val pages = listOf(
+        EditDataCustomerFragment(),
+        EditWarehouseCustomerFragment()
+    )
+    private var currentPosition: Int = 0
+
+    fun getCurrentFragment() = pages[currentPosition-1]
+
+    override fun getItem(position: Int): Fragment {
+        currentPosition = position
+        return pages[position]
+    }
+
+    override fun getCount(): Int {
+        return pages.size
+    }
+
+    override fun getPageTitle(position: Int): CharSequence? {
+        return pages[position].tagName
+    }
 }
