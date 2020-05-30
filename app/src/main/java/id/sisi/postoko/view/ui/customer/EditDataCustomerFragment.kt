0e6346412e3c02a3df9a027848ed2cf82230caa8
asthok_aripasola <arpasola22@gmail.com>
2020-05-30 08:30:38 +0700

    POSMA-230 #time 5h Memperbaiki error pemasangan spinner dan memperbaiki kondisi spinner daerah yang awalnya masih static dengan 2 ada di ganti dengan yang dinamis dan menggunakan api, kendala yang bikin lama adalah error force close yan tidak tau sebabnya karena android debug saya masi tidak dapt memunculkan log

--- app/src/main/java/id/sisi/postoko/view/ui/customer/EditDataCustomerFragment.kt
+++ app/src/main/java/id/sisi/postoko/view/ui/customer/EditDataCustomerFragment.kt
@@ -8,6 +8,7 @@ import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import android.widget.AdapterView
+import android.widget.ArrayAdapter
 import android.widget.RadioButton
 import android.widget.Toast
 import androidx.core.view.get
@@ -24,21 +25,24 @@ import id.sisi.postoko.utils.MySpinnerAdapter
 import id.sisi.postoko.utils.extensions.setIfExist
 import id.sisi.postoko.view.BaseFragment
 import id.sisi.postoko.view.custom.CustomProgressBar
+import id.sisi.postoko.view.ui.daerah.DaerahViewModel
 import kotlinx.android.synthetic.main.fragment_edit_data_customer.*
 import kotlinx.android.synthetic.main.fragment_edit_data_customer.view.*
 import java.util.ArrayList
 
 class EditDataCustomerFragment : BaseFragment() {
     private lateinit var viewModelCustomer: CustomerViewModel
+    private lateinit var mViewModelDaerah: DaerahViewModel
+
     private var listCustomerGroup: List<CustomerGroup> = ArrayList()
     private var listPriceGroup: List<PriceGroup> = ArrayList()
     private var idCustomerGroup: String? = null
     private var idPriceGroup: String? = null
     private val progressBar = CustomProgressBar()
     private var customer: Customer? = null
-    private val listProvinsi = mutableListOf<DataSpinner>()
-    private val listKabupaten = mutableListOf<DataSpinner>()
-    private val listKecamatan = mutableListOf<DataSpinner>()
+    private var provinceList: Array<String> = arrayOf()
+    private var cityList: Array<String> = arrayOf()
+    private var villageList: Array<String> = arrayOf()
 
     companion object {
         fun newInstance() = EditDataCustomerFragment()
@@ -72,7 +76,7 @@ class EditDataCustomerFragment : BaseFragment() {
         view.et_cf4_edit_customer.setText(customer?.cf4)
         view.et_cf5_edit_customer.setText(customer?.cf5)
 
-        /*view.rg_status_edit_customer
+        view.rg_status_edit_customer
         val adapterCustomerGroup = MySpinnerAdapter(activity!!.applicationContext, android.R.layout.simple_spinner_dropdown_item)
 
         viewModelCustomer = ViewModelProvider(this).get(CustomerViewModel::class.java)
@@ -82,7 +86,7 @@ class EditDataCustomerFragment : BaseFragment() {
                 adapterCustomerGroup.udpateView(it.map {cg->
                     return@map DataSpinner(cg.name, cg.id)
                 }.toMutableList())
-                view.sp_customer_group_edit_customer.setIfExist(customer?.customer_group_id.toString())
+                /*view.sp_customer_group_edit_customer.setIfExist(customer?.customer_group_id.toString())*/
                 listCustomerGroup = it
             }
         })
@@ -124,28 +128,74 @@ class EditDataCustomerFragment : BaseFragment() {
             override fun onNothingSelected(parent: AdapterView<*>?) = Unit
         }
 
-        listProvinsi.add(DataSpinner("NANGGROE ACEH DARUSSALAM", "NANGGROE ACEH DARUSSALAM"))
+        view.sp_provinsi_group_edit_customer.onItemSelectedListener = object : AdapterView.OnItemSelectedListener {
+            override fun onNothingSelected(parent: AdapterView<*>?) {}
+            override fun onItemSelected(parent: AdapterView<*>?, view: View?, position: Int, id: Long) {
+                mViewModelDaerah.getCity(provinceList[position])
+            }
+        }
+
+        view.sp_city_group_edit_customer.onItemSelectedListener = object : AdapterView.OnItemSelectedListener {
+            override fun onNothingSelected(parent: AdapterView<*>?) {}
+            override fun onItemSelected(parent: AdapterView<*>?, view: View?, position: Int, id: Long) {
+                mViewModelDaerah.getStates(cityList[position])
+            }
+        }
+
+        mViewModelDaerah = ViewModelProvider(this).get(DaerahViewModel::class.java)
+
+        mViewModelDaerah.getAllProvince().observe(viewLifecycleOwner, Observer {
+            it?.let {listDataDaerah ->
+                provinceList = listDataDaerah.map {dataDaerah ->
+                    return@map dataDaerah.province_name ?: ""
+                }.toTypedArray()
+            }
+            setUIProvince(view)
+        })
+
+        mViewModelDaerah.getAllCity().observe(viewLifecycleOwner, Observer {
+            it?.let {listDataDaerah ->
+                cityList = listDataDaerah.map {dataDaerah ->
+                    return@map dataDaerah.kabupaten_name ?: ""
+                }.toTypedArray()
+            }
+            setUICity(view)
+        })
+
+        mViewModelDaerah.getAllStates().observe(viewLifecycleOwner, Observer {
+            it?.let {listDataDaerah ->
+                villageList = listDataDaerah.map {dataDaerah ->
+                    return@map dataDaerah.kecamatan_name ?: ""
+                }.toTypedArray()
+            }
+            setUIStates(view)
+        })
+
+        mViewModelDaerah.getProvince()
+
+
+        /*listProvinsi.add(DataSpinner("NANGGROE ACEH DARUSSALAM", "NANGGROE ACEH DARUSSALAM"))
         listProvinsi.add(DataSpinner("SUMATERA UTARA", "SUMATERA UTARA"))
 
         val adapterProvonsi = MySpinnerAdapter(activity!!.applicationContext, android.R.layout.simple_list_item_1, listProvinsi)
         view.sp_provinsi_group_edit_customer.adapter = adapterProvonsi
-        view.sp_provinsi_group_edit_customer.setIfExist(customer?.country ?: null.toString())
+        //view.sp_provinsi_group_edit_customer.setIfExist(customer?.country ?: null.toString())
 
         listKabupaten.add(DataSpinner("ACEH SINGKIL", "ACEH SINGKIL"))
         listKabupaten.add(DataSpinner("PIDIE", "PIDIE"))
 
         val adapterKabupaten = MySpinnerAdapter(activity!!.applicationContext, android.R.layout.simple_list_item_1, listKabupaten)
         view.sp_city_group_edit_customer.adapter = adapterKabupaten
-        view.sp_city_group_edit_customer.setIfExist(customer?.city ?: null.toString())
+        //view.sp_city_group_edit_customer.setIfExist(customer?.city ?: null.toString())
 
         listKecamatan.add(DataSpinner("SINGKOHOR", "SINGKOHOR"))
         listKecamatan.add(DataSpinner("LAWE ALAS", "LAWE ALAS"))
 
         val adapterKecamatan = MySpinnerAdapter(activity!!.applicationContext, android.R.layout.simple_list_item_1, listKecamatan)
         view.sp_district_group_edit_customer.adapter = adapterKecamatan
-        view.sp_district_group_edit_customer.setIfExist(customer?.state ?: null.toString())
+        //view.sp_district_group_edit_customer.setIfExist(customer?.state ?: null.toString())*/
 
-        for (i in 0 until (rg_status_edit_customer?.childCount ?: 0)) {
+        for (i in 0 until (view.rg_status_edit_customer?.childCount ?: 0)) {
             (view.rg_status_edit_customer?.get(i) as? RadioButton)?.tag = i
         }
 
@@ -153,48 +203,78 @@ class EditDataCustomerFragment : BaseFragment() {
             val radioButton = radioGroup.findViewById<RadioButton>(i)
             view.rg_status_edit_customer?.tag = radioButton.tag
         }
-        view.rg_status_edit_customer?.check(rg_status_edit_customer?.get(1)?.id ?: 0)*/
+        view.rg_status_edit_customer?.check(view.rg_status_edit_customer?.get(1)?.id ?: 0)
 
         view.btn_confirmation_edit_customer.setOnClickListener {
-            actionEditCustomer()
+            actionEditCustomer(view)
         }
 
         return view
     }
 
-    private fun actionEditCustomer() {
+    private fun setUIProvince(view2: View){
+        view2.sp_provinsi_group_edit_customer.adapter = context?.let {
+            ArrayAdapter(
+                it,
+                R.layout.support_simple_spinner_dropdown_item,
+                provinceList
+            )
+        }
+    }
+
+    private fun setUICity(view2: View){
+        view2.sp_city_group_edit_customer.adapter = context?.let {
+            ArrayAdapter(
+                it,
+                R.layout.support_simple_spinner_dropdown_item,
+                cityList
+            )
+        }
+    }
+
+    private fun setUIStates(view2: View){
+        view2.sp_district_group_edit_customer.adapter = context?.let {
+            ArrayAdapter(
+                it,
+                R.layout.support_simple_spinner_dropdown_item,
+                villageList
+            )
+        }
+    }
+
+    private fun actionEditCustomer(view2: View) {
         val numbersMap =  validationFormEditCustomer()
 
         if (numbersMap["type"] as Boolean){
             progressBar.show(activity!!.applicationContext, "Silakan tunggu...")
 
             val body: MutableMap<String, Any> = mutableMapOf(
-                "name" to (view!!.et_name_edit_customer?.text?.toString() ?: ""),
-                "email" to (view!!.et_email_edit_customer?.text?.toString() ?: ""),
+                "name" to (view2.et_name_edit_customer?.text?.toString() ?: ""),
+                "email" to (view2.et_email_edit_customer?.text?.toString() ?: ""),
                 "customer_group_id" to (idCustomerGroup ?: ""),
                 "price_group_id" to (idPriceGroup ?: ""),
-                "company" to (view!!.et_company_name_edit_customer?.text?.toString() ?: ""),
-                "address" to (view!!.et_address_edit_customer?.text?.toString() ?: ""),
-                "vat_no" to (view!!.et_npwp_edit_customer?.text?.toString() ?: ""),
-                "postal_code" to (view!!.et_postal_code_edit_customer?.text?.toString() ?: ""),
-                "phone" to (view!!.et_phone_edit_customer?.text?.toString() ?: ""),
-                "cf1" to (view!!.et_cf1_edit_customer?.text?.toString() ?: ""),
-                "cf2" to (view!!.et_cf2_edit_customer?.text?.toString() ?: ""),
-                "cf3" to (view!!.et_cf3_edit_customer?.text?.toString() ?: ""),
-                "cf4" to (view!!.et_cf4_edit_customer?.text?.toString() ?: ""),
-                "cf5" to (view!!.et_cf5_edit_customer?.text?.toString() ?: ""),
-                "provinsi" to (view!!.sp_provinsi_group_edit_customer?.selectedItem?.toString() ?: ""),
-                "kabupaten" to (view!!.sp_district_group_edit_customer?.selectedItem?.toString() ?: ""),
-                "kecamatan" to (view!!.sp_city_group_edit_customer?.selectedItem?.toString() ?: "")
+                "company" to (view2.et_company_name_edit_customer?.text?.toString() ?: ""),
+                "address" to (view2.et_address_edit_customer?.text?.toString() ?: ""),
+                "vat_no" to (view2.et_npwp_edit_customer?.text?.toString() ?: ""),
+                "postal_code" to (view2.et_postal_code_edit_customer?.text?.toString() ?: ""),
+                "phone" to (view2.et_phone_edit_customer?.text?.toString() ?: ""),
+                "cf1" to (view2.et_cf1_edit_customer?.text?.toString() ?: ""),
+                "cf2" to (view2.et_cf2_edit_customer?.text?.toString() ?: ""),
+                "cf3" to (view2.et_cf3_edit_customer?.text?.toString() ?: ""),
+                "cf4" to (view2.et_cf4_edit_customer?.text?.toString() ?: ""),
+                "cf5" to (view2.et_cf5_edit_customer?.text?.toString() ?: ""),
+                "provinsi" to (view2.sp_provinsi_group_edit_customer?.selectedItem?.toString() ?: ""),
+                "kabupaten" to (view2.sp_city_group_edit_customer?.selectedItem?.toString() ?: ""),
+                "kecamatan" to (view2.sp_district_group_edit_customer?.selectedItem?.toString() ?: "")
             )
             customer?.id?.let { viewModelCustomer.setIdCustomer(it) }
             viewModelCustomer.postEditSale(body){
                 progressBar.dialog.dismiss()
                 Toast.makeText(activity!!.applicationContext, ""+it["message"], Toast.LENGTH_SHORT).show()
                 if (it["networkRespone"]?.equals(NetworkResponse.SUCCESS)!!) {
-                    val returnIntent = Intent()
+                    /*val returnIntent = Intent()
                     activity!!.setResult(Activity.RESULT_OK, returnIntent)
-                    activity!!.finish()
+                    activity!!.finish()*/
                 }
             }
         }else{
@@ -241,12 +321,12 @@ class EditDataCustomerFragment : BaseFragment() {
             cek = false
         }
 
-        if (view!!.sp_district_group_edit_customer?.selectedItem.toString() == ""){
+        if (view!!.sp_city_group_edit_customer?.selectedItem.toString() == ""){
             message += "- Kabupaten/Kota Tidak Boleh Kosong\n"
             cek = false
         }
 
-        if (view!!.sp_city_group_edit_customer?.selectedItem.toString() == ""){
+        if (view!!.sp_district_group_edit_customer?.selectedItem.toString() == ""){
             message += "- Kecamatan Tidak Boleh Kosong\n"
             cek = false
         }
