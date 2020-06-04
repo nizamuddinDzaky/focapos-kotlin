63425b0361172eaaf5f41e78f3033b6c3ba9eae0
asthok_aripasola <arpasola22@gmail.com>
2020-06-04 11:08:56 +0700

    POSMA-201 #time 1h 45m Cek feedback, action button di tiap halaman hanya boleh 1x aksi, konfirmasi ubah atau tambah keluar tiap halaman transaksi, loading progress di tiap halaman yang request api

--- app/src/main/java/id/sisi/postoko/view/ui/gr/GoodReceivedViewModel.kt
+++ app/src/main/java/id/sisi/postoko/view/ui/gr/GoodReceivedViewModel.kt
@@ -27,12 +27,13 @@ class GoodReceivedViewModel(var status: String) : ViewModel() {
                 goodsReceived.postValue(null)
             },
             onResponse = { _, response ->
-                isExecute.postValue(false)
+                isExecute.postValue(true)
                 if (response.isSuccessful) {
                     tryMe {
                         goodsReceived.postValue(response.body()?.data?.list_goods_received)
                     }
                 } else {
+                    isExecute.postValue(false)
                     goodsReceived.postValue(listOf())
                 }
             }
